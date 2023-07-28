a48773fe2638284c64d6f5a6af1f5a0bbec112af
Danilo César <danilocesar@outlook.com>
2023-07-27 23:00:14 -0300

    feat: Adiciona rotas para métodos de Gerar hash

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, request
 import json
 
+from services.AddressHash import AddressHash
 from services.FindCoordinates import FindCoordinates
 from settings import SECRET_KEY
 
@@ -28,6 +29,24 @@ def api_find():
     return jsonify(
         coordinates_service.get_coordinates_from_address(address=address)
     )
+
+## API: Gerar novo CEP
+@app.route('/api/gerar', methods=['GET'])
+def api_gerar():
+    coordinates_service = FindCoordinates()
+    hash_service = AddressHash()
+
+    user_address = request.form.get("address")
+    user_landmark = request.form.get("landmark")
+    coordinates_response = coordinates_service.get_coordinates_from_address(address=user_address)
+
+    hash_result = {}
+    if (("success" in coordinates_response) and (coordinates_response["success"] is True)):
+        coordinates_dict = coordinates_response["result"]
+        hash_result = hash_service.generate_hash(coordinate_lat=coordinates_dict["lat"], coordinate_lon=coordinates_dict["lon"], address_landmark=user_landmark, export_binary=False)
+
+    return jsonify(hash_result)
+
 
 # Run
 
